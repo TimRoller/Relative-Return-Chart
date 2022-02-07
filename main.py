6859173d928fad66d4a803e9709501d9b0fb570e
TimRoller <timroller@gmail.com>
2022-02-07 15:03:35 -0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import pandas as pd
 
 start = dt.datetime(2021,1,1)
 end = dt.datetime.now()
-Tickers = ['^GSPC', '1INCH-USD', 'ETH-USD','BTC-USD','BAT-USD','UNI1-USD', 'ATOM-USD', 'AAVE-USD', 'DOGE-USD', 'MATIC-USD']
+Tickers = ['SPY', '1INCH-USD', 'ETH-USD','BTC-USD','BAT-USD','UNI1-USD', 'ATOM-USD', 'AAVE-USD', 'DOGE-USD', 'MATIC-USD']
+Labels = ['S&P500','1INCH', 'ETH','BTC','BAT','UNI', 'ATOM', 'AAVE', 'DOGE', 'MATIC']
 Returns = {}
 
 fig = go.Figure()
@@ -15,7 +16,7 @@ Returns = pd.DataFrame(columns=['Ticker', 'Return', 'Drawdown'])
 for index,value in enumerate(Tickers):
     df = web.DataReader(value, 'yahoo', start, end)
 
-    fig.add_trace(go.Scatter(x=df.index, y=(100*df['Adj Close']/(df['Adj Close'][0])-100),name=value))
+    fig.add_trace(go.Scatter(x=df.index, y=round(100*df['Adj Close']/(df['Adj Close'][0])-100,0),name=Labels[index]))
     df['daily_returns'] = (df['Adj Close'] / df['Adj Close'].shift(1)) - 1
 
     # Drop all Not a number values using drop method.
@@ -39,6 +40,13 @@ Returns['Drawdown'] = Returns['Drawdown'].map('{:,.0f}%'.format)
 fig.update_traces(mode="lines", hovertemplate=None)
 fig.update_layout(hovermode="x")
 fig.update_layout(yaxis_type="log")
+fig.update_layout(title='Total % Return since 2021 on Log Scale')
+fig.update_layout(legend=dict(
+    yanchor="top",
+    y=0.99,
+    xanchor="left",
+    x=0.01
+))
 
 print (Returns)
 fig.show()
